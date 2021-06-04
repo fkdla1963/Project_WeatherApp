ce4b52a4c77d944cfddbb1184c854bf0d4868540
pkh2351 <76101223+fkdla1963@users.noreply.github.com>
2021-06-04 10:11:25 +0900

    버튼 오류 수정

--- app/src/main/java/com/kr/weathertest/MainActivity.java
+++ app/src/main/java/com/kr/weathertest/MainActivity.java
@@ -4,24 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Object View;
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.Button1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Toast.makeText(MainActivity.this, "버튼 수정입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
-    Button btn = (Button)findViewById(R.id.Button1);
 
-    btn.setOnClickListener(new  void OnClickListener() {
 
-        public void onClick(View v){
-            startActivity(new Intent(MainActivity.this, MainActivity2.class));
-        }
-    };
+
 
 }
